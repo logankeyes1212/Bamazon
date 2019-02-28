567cca6b68c8a1a33f791e9e58a01f3413411d55
Logan <logankeyes1212@gmail.com>
2019-02-27 18:50:43 -0600

    stock is working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -28,7 +28,7 @@ function start() {
     // console.log(query)
     connection.query(query, function (err, res) {
         //    console.log(res) 
-
+        // creating empty array for products
         let choices = [];
         for (i = 0; i < res.length; i++) {
             choices.push({ id: res[i].id, product: res[i].product_name, price: "$" + res[i].price })
@@ -38,7 +38,7 @@ function start() {
 
     })
 }
-
+// items to buy function
 function itemToBuy() {
 
     inquirer.prompt({
@@ -48,19 +48,46 @@ function itemToBuy() {
 
     }).then(function (selected) {
         selected = parseInt(selected.id)
-        console.log(selected)
+        console.log("you selected item #" + selected)
+        // pulling items from sql
         var query = "SELECT * FROM products WHERE id =" + selected;
+
         connection.query(query, { id: selected }, function (err, res) {
             //    if(err) throw err;
             for (var i = 0; i < res.length; i++) {
                 // res.push({ id: res[i].id, product: res[i].product_name, department: res[i].department_name })
-
+                var product = res[i].product_name;
+                var stock = res[i].stock_quantity;
+                console.log(product)
                 console.log({
-                           id: res[i].id,
-                      product: res[i].product_name,
-                   department: res[i].department_name
+                    id: res[i].id,
+                    product: res[i].product_name,
+                    department: res[i].department_name,
+                    stock_quantity: res[i].stock_quantity
                 })
-                quantity();
+                    quantity();
+               
+                function quantity() {
+                    if (stock >= 1 ){
+                    inquirer.prompt({
+                        name: "stock_quantity",
+                        type: "input",
+                        message: "Please enter the quantity you would like to buy"
+                    }).then(function (data) {
+                        data = parseInt(data.stock_quantity)
+                        console.log(stock)
+                        console.log("you are buying " + data + " " + product + "s")
+                        var query = "UPDATE products SET stock_quantity = (stock_quantity - " + data + ") WHERE id =" + selected;
+                        // console.log(query)
+                        connection.query(query, function (err, res) {
+                           
+                            // if (data.stock_quantity === 0)
+                            // console.log("there is not enough" + product + "s left")
+                        
+                        })
+                    })
+                }else console.log("there is not enough "+ product +"s in our inventory")
+                }
             }
         })
     });
@@ -70,13 +97,10 @@ function itemToBuy() {
 //         name: "stock_quantity",
 //         type: "input",
 //         message: "Please enter the quantity you would like to buy"
-//     }).then(function (selected) {
-//         selected = parseInt(selected.stock_quantity)
-//         console.log(selected)
-//         var query = "UPDATE products SET stock_quantity = (stock_quantity - "+selected+"where id ="
+//     }).then(function (data) {
+//         data = parseInt(data.stock_quantity)
+//         console.log(data)
+//         var query = "UPDATE products SET stock_quantity = (stock_quantity - "+data+"where id ="
 //         connection.query(query)
 //     })
 // }
-UPDATE products
-SET stock_quantity = (stock_quantity - 1) 
-WHERE Id = 11;
